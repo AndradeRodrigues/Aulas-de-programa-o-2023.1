16304ffbf1bb5c2ccb5583412d307dc0f365ed11
Marcelo Rodrigues de Andrade <129122144+AndradeRodrigues@users.noreply.github.com>
2023-04-12 15:19:49 -0300

    Add files via upload
    Commit de objetos Built-in

---
+++ Objetos Built-in.py
@@ -0,0 +1,27 @@
+#Elementos tipo built-in
+
+S = "spam"
+print(len(S))  #mostrar o tamanho de s
+print(S[0])  #mostrando  o primeiro índicde de posição 0
+print(
+  S[1:3])  # definindo um intervalo que começa posição 1 e vai até a posição 2
+print(S + 'xyz')
+#S[0]
+#S[1:3]
+#S + ‘xyz'
+#S.find(‘pa')
+#S.upper()
+#S.isalpha()
+#S.replace(‘pa’,'ca')
+
+#L = [123, ‘casa’, 3.14]
+#len(L)
+#L[0]
+#L * 2
+#L.append(‘bola’)
+
+#M = [[1,2,3],[4,5,6],[7,8,9]]
+#col2 = [n[1] for n in M]
+#D = {‘com’:’ABC', ‘num’:23}
+#D[‘num’]
+#D[‘cor’] = ’azul'
