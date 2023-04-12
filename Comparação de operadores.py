f1d44d08e04b322a5f44d0a80b65cbd14c23aba1
Marcelo Rodrigues de Andrade <129122144+AndradeRodrigues@users.noreply.github.com>
2023-04-12 15:37:09 -0300

    Add files via upload
    Comentando a comparação de operadores com alguns exemplos

---
+++ Comparação de operadores.py
@@ -0,0 +1,17 @@
+#Operadores de comparação
+#Operadores que trabalham no nível de bits:
+#>>> x = 2
+#>>> y = 4
+#>>> z = 6
+
+#Comparação
+#>>> x < y
+#True
+
+#Comparação
+#>>> z < y
+
+#Comparação
+#>>> x < y < z #Equivalente a (x < y) and (y <z)
+#True
+
