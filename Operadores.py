e0ec3caf5144f9c33f6fcdf82d4a49fefbddeda9
Marcelo Rodrigues de Andrade <129122144+AndradeRodrigues@users.noreply.github.com>
2023-04-12 15:29:50 -0300

    Add files via upload
    Commit referente ao uso de operadores e ordem de selação.

---
+++ Operadores.py
@@ -0,0 +1,9 @@
+#Operadores
+print(40 + 3.14)  #Conversão para float antes da operação
+print(40 + int(3.14))  #Conversão explícita
+
+#Diferença enre converversão para int e arredondamento
+
+print(int(3.77))
+print(round(3.77))
+print(round(3.49, 2))
